de8f9ecf41b0ca0309a3dd62f31a4d693c29db12
c-o-b-a-x <cobyhughs2008@gmail.com>
2026-01-07 13:43:52 -0500

    Revert "Add greeting message and comment in app.py"
    This reverts commit 119c9254d486a847588a23231e789e663793f52b.

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+<<<<<<< HEAD
 print ("Git/Github Demo")
 #some comment
 print ("this is an okay day-remote")
 print ("this is an okay day-local")
 print ("This is a great day")
+=======
+print ("Git/Github Demo")
+>>>>>>> parent of 119c925 (Add greeting message and comment in app.py)
